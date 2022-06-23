610a29826fa7a60c48dfff9fd1c06e0b875bcbda
thelonelydeveloper.real@gmail.com <thelonelydeveloper.real@gmail.com>
2022-06-23 10:58:09 +0530

    hyy

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ console.log("hello")
 window.onclick = () => {
 	window.location.replace = "https://google.com"
 }
+document.querySelector('button').onclick=function(){
+	window.locationbar.replace("http://thelonelydev123.github.io/TheLonelyDev123/Test")
+}
